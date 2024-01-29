ac8740344f63b1f6c2b53da72482fe530e46709b
awais <se.awaissaleem@gmail.com>
2024-01-29 12:25:54 +0500

    pass data through api in state link

--- src/componts/card.jsx
+++ src/componts/card.jsx
@@ -1,13 +1,40 @@
-import React, { useState } from 'react'
-import {peopleArray} from './data'
+import  { useEffect, useState } from 'react'
+// import {peopleArray} from './data'
+// import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import { Link } from 'react-router-dom'
 const Cards = () => {
-    const [list, setList]=useState(peopleArray)
-let remove=()=>{
-        setList(list.slice(0,-1))
-        // console.log(setList(list[0]))
-        // list.slice(0,-1)
-      }
+
+
+useEffect(()=>{
+  const apidata=fetch('https://randomuser.me/api/?results=10')
+  apidata.then((res)=>res.json()).then((responce)=>{
+    let desture=responce.results;
+    setList(desture)
+  })
+},[])
+     
+    const [list, setList]=useState([])
+    const [current,setcurrentindex]=useState(null)
+   const [show, setshow]=useState(false)
+   const handleShow=(index)=>{
+    setshow(true)
+    setcurrentindex(index)
+    
+   }
+   const handleClose=()=>setshow(false)
+  
+  
+   const remove=()=>{
+           console.log("clicked")
+           let updated=[...list]
+           updated.splice(current,1)
+           setList(updated)
+           handleClose()
+          
+}
+
                   
   return (
     <div>
@@ -17,25 +44,38 @@ let remove=()=>{
           <p className='py-5 text-1xl text-black'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui totam amet temporibus, delectus quos excepturi enim deserunt et molestiae eos, odio aliquam eaque dignissimos voluptatum ullam, est obcaecati alias ipsam.</p>
         </div>
         {
-            peopleArray.map((e)=>{
+        
+          list.map((e,index)=>{
             return(
-              <Link to={`details/${e.id}`} >
+              <div className='relative'>
+              <Link to={`details/${e.id.name}`} state={{data:e}}>
                 
                 <div className='max-w-7xl h-52 py-4 px-32  mx-auto  bg-slate-200 dpy-20 px-10 border-2 border-solid border-slate-300'>
                 <div className='flex'>
-                 <h1 className='h-15 w-14 '><img src={`${e.image}`} alt="" className='rounded-full' /></h1>
-                 <h1 className='text-4xl px-7'>{e.name}</h1>
-                 <span className='py-10'>{e.address}</span>
+                 <h1 className='h-15 w-14 myimg '><img src={`${e.picture.large}`} alt="" className='rounded-full' /></h1>
+                 <h1 className='text-4xl px-7'>{e.name.first}</h1>
+                 <span className='py-10'>{e.email}</span>
 
                 </div> 
                  <div>
-                 <p className='text-2xl font-bold'>{e.profession}</p>
+                 <p className='text-2xl font-bold'>{e.name.last}</p>
                   </div>
               
-              <button onClick={remove} className='bg-slate-600 text-white'>Remove</button>
               </div>
 
               </Link> 
+              <Button variant='primary' onClick={()=>handleShow(index)} className='bg-slate-600 text-white absolute bottom-12 right-14 '>Remove</Button>
+              <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+        <Button variant='primary' onClick={()=>remove()}>Deleted</Button>
+        <Button variant='secondary' onClick={handleClose}>Save changes</Button>
+        </Modal.Footer>
+      </Modal>
+              </div>
             )
             })
 
@@ -45,4 +85,4 @@ let remove=()=>{
   )
 }
 
-export default Cards
+export default Cards;
--- src/pages/details.jsx
+++ src/pages/details.jsx
@@ -3,26 +3,25 @@
 
 
 import React from 'react'
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import {peopleArray} from '../componts/data'
 
 const Detalis = () => {
-                              
-    const {id} =useParams()
+             
+            let location=useLocation();
+           let description =location.state.data
 
-      let  description=peopleArray.find((e)=>{
-        return e.id==id
-       })
+
   return (
     <>
       <div className=' max-w-7xl m-auto flex h-screen bg-cyan-100 py-20 px-20'>
         <div className='basis-[45%]'>
-           <img src={`${description.image}`} alt="" className='rounded-2xl h-[100%]' />
+           <img src={`${description.picture.large}`} alt="" className='rounded-2xl h-[100%]' />
         </div>
         <div className='basis-[50%] py-10 px-10'>
-          <h1 className='text-5xl font-bold text-black py-5'>{description.name}</h1>
-          <p className='text-2xl text-black py-5'>{description.address}</p>
-          <p className='text-1xl text-black font-bold'>{description.profession}</p>
+          <h1 className='text-5xl font-bold text-black py-5'>{description.first}</h1>
+          <p className='text-2xl text-black py-5'>{description.email}</p>
+          <p className='text-1xl text-black font-bold'>{description.location.city}</p>
         </div>
       </div>
     </>
